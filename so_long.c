676ceaec2eee0fb8c86f4b84ffb17ee989b5e5a2
Jules MUNTZ-BERGER <julmuntz@e1r7p13.clusters.42paris.fr>
2022-08-17 18:03:18 +0200

    1st pixel displayed

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 16:39:03 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/08/17 17:18:44 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/08/17 17:58:52 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 	t_data	img;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "test");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
+	ft_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
