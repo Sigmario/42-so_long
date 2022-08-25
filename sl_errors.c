b711c737dc47262b36a0949d66b274ee3f7f2bc2
Jules MUNTZ-BERGER <julmuntz@e2r8p2.clusters.42paris.fr>
2022-08-25 18:04:30 +0200

    Contextual error messages

--- sl_errors.c
+++ sl_errors.c
@@ -6,27 +6,41 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 17:51:18 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/08/23 19:10:31 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/08/25 17:53:55 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	sl_char_errors(t_data data)
+int	sl_char_errors_2(int count_exit, int count_coll, int count_play)
 {
-	static int	line;
-	int			count_exit;
-	int			count_coll;
-	int			count_play;
+	if (count_exit < 1 || count_coll < 1 || count_play < 1)
+	{
+		ft_printf("Error\n");
+		if (count_exit < 1)
+			ft_printf("No exit found.\n");
+		if (count_coll < 1)
+			ft_printf("No collectible found.\n");
+		if (count_play < 1)
+			ft_printf("No player found.\n");
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+int	sl_char_errors_1(t_data data)
+{
+	static int	count_exit;
+	static int	count_coll;
+	static int	count_play;
+	int			line;
 	int			i;
 
-	count_exit = 0;
-	count_coll = 0;
-	count_play = 0;
-	while (data.map[++line])
+	line = 0;
+	while (data.map[line])
 	{
 		i = 0;
-		while (data.map[line][++i])
+		while (data.map[line][i])
 		{
 			if (data.map[line][i] == 'E')
 				count_exit++;
@@ -34,36 +48,40 @@ int	sl_char_errors(t_data data)
 				count_coll++;
 			if (data.map[line][i] == 'P')
 				count_play++;
+			i++;
 		}
+		line++;
 	}
-	if (count_exit < 1 || count_coll < 1 || count_play < 1)
+	if (sl_char_errors_2(count_exit, count_coll, count_play) == TRUE)
 		return (TRUE);
 	return (FALSE);
 }
 
 int	sl_errors(t_data data)
 {
-	static int	line;
+	int	line;
 	int	i;
 
-	if (sl_char_errors(data) == TRUE)
-		return (TRUE);
-	while (data.map[++line])
+	line = 0;
+	while (data.map[line])
 	{
 		if (ft_linelen(data.map[line]) != ft_linelen(data.map[0]))
-			return (TRUE);
+			return (ft_printf("Error\nNot rectangular.\n"));
 		i = 0;
 		while (data.map[line][i])
 		{
 			if ((data.map[0][i] != '1' && data.map[0][i] != '\n')
 				|| (data.map[data.nb_line - 1][i] != '1'
 					&& data.map[data.nb_line - 1][i] != '\0'))
-				return (TRUE);
+				return (ft_printf("Error\nAperture(s) found in walls.\n"));
 			if (data.map[line][0] != '1'
 				|| data.map[line][ft_linelen(data.map[line] + 1)] != '1')
-				return (TRUE);
+				return (ft_printf("Error\nAperture(s) found in walls.\n"));
 			i++;
 		}
+		line++;
 	}
+	if (sl_char_errors_1(data) == TRUE)
+		return (TRUE);
 	return (FALSE);
 }
--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 16:39:03 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/08/24 18:51:50 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/08/25 16:51:04 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 int	main(void)
 {
-	static int i;
-	t_data data;
-	void *mlx;
-	void *mlx_win;
-	t_data img;
+	static int	i;
+	t_data		data;
+	void		*mlx;
+	void		*mlx_win;
+	t_data		img;
 
 	mlx = mlx_init();
-
 	data.map = sl_get_map(&data);
-	if (sl_errors(data) == TRUE)
-		return (ft_printf("\nError: Invalid Map\n"));
-	while (data.map[i])
-		ft_printf("%s", data.map[i++]);
-	ft_printf("\n\nMap Size:\t%d x %d\n", data.nb_line, data.nb_char);
-	free(data.map);
-
-	mlx_win = mlx_new_window(mlx, 500, 300, "test");
-	img.img = mlx_new_image(mlx, 100, 100);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-	img.img = mlx_xpm_file_to_image(mlx, "./images/w1.xpm", &data.h, &data.w);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, data.x, data.y);
-	
-	mlx_loop(mlx);
+	if (sl_errors(data) == FALSE)
+	{
+		while (data.map[i])
+			ft_printf("%s", data.map[i++]);
+		ft_printf("\n\nMap Size:\t%d x %d\n", data.nb_line, data.nb_char);
+		free(data.map);
+		mlx_win = mlx_new_window(mlx, 500, 300, "test");
+		img.img = mlx_new_image(mlx, 100, 100);
+		img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
+		// img.img = mlx_xpm_file_to_image(mlx, "./images/w1.xpm", &data.h, &data.w);
+		mlx_put_image_to_window(mlx, mlx_win, img.img, data.x, data.y);
+		mlx_loop(mlx);
+	}
+	else
+		return (TRUE);
 	return (0);
 }
