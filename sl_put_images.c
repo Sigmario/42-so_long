84134729855784f15b20155c7c4c35a9dabdff16
Jules MUNTZ-BERGER <julmuntz@e1r1p3.clusters.42paris.fr>
2022-08-27 12:05:07 +0200

    Player sprite found

--- sl_put_images.c
+++ sl_put_images.c
@@ -6,33 +6,39 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 19:54:40 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/08/26 21:19:25 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/08/27 11:45:16 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	sl_find_images(t_data *data)
+{
+	data->play_image = mlx_xpm_file_to_image(data->mlx, "./images/w1.xpm", &(data->w), &(data->h));
+}
 
 void	sl_put_images(t_data *data)
 {
 	int	line;
 	int	i;
 
+	sl_find_images(data);
 	line = -1;
 	while (data->map[++line])
 	{
 		i = -1;
 		while (data->map[line][++i])
 		{
-			if (data->map[line][i] == '0')
-				mlx_put_image_to_window(data->mlx, data->mlx_win, data->void_image, (line * 33), (i * 37));
-			if (data->map[line][i] == '1')
-				mlx_put_image_to_window(data->mlx, data->mlx_win, data->wall_image, (line * 33), (i * 37));
-			if (data->map[line][i] == 'E')
-				mlx_put_image_to_window(data->mlx, data->mlx_win, data->exit_image, (line * 33), (i * 37));
-			if (data->map[line][i] == 'C')
-				mlx_put_image_to_window(data->mlx, data->mlx_win, data->coll_image, (line * 33), (i * 37));
+			// if (data->map[line][i] == '0')
+			// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->void_image, (i * 33), (line * 37));
+			// if (data->map[line][i] == '1')
+			// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->wall_image, (i * 33), (line * 37));
+			// if (data->map[line][i] == 'E')
+			// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->exit_image, (i * 33), (line * 37));
+			// if (data->map[line][i] == 'C')
+			// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->coll_image, (i * 33), (line * 37));
 			if (data->map[line][i] == 'P')
-				mlx_put_image_to_window(data->mlx, data->mlx_win, data->play_image, (line * 33), (i * 37));
+				mlx_put_image_to_window(data->mlx, data->mlx_win, data->play_image, (i * 33), (line * 37));
 		}
 	}
 }
--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 16:39:03 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/08/25 21:11:47 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/08/27 11:53:11 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@ int	main(void)
 {
 	static int	i;
 	t_data		data;
-	void		*mlx;
-	void		*mlx_win;
-	t_data		img;
-
-	mlx = mlx_init();
+	
+	data.mlx = mlx_init();
 	data.map = sl_get_map(&data);
 	if (sl_errors(data) == FALSE)
 	{
@@ -30,14 +27,10 @@ int	main(void)
 			return (TRUE);
 		while (data.map[i])
 			ft_printf("%s", data.map[i++]);
-		ft_printf("\n\nMap Size:\t%d x %d\n", data.nb_line, data.nb_char);
-		free(data.map);
-		mlx_win = mlx_new_window(mlx, 500, 300, "test");
-		img.img = mlx_new_image(mlx, 100, 100);
-		img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-		// img.img = mlx_xpm_file_to_image(mlx, "./images/w1.xpm", &data.h, &data.w);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, data.x, data.y);
-		mlx_loop(mlx);
+		ft_printf("\n\nMap Size:\tHeight:\t%d\n\t\tWidth:\t%d\n", data.nb_line, data.nb_char);
+		data.mlx_win = mlx_new_window(data.mlx, 500, 300, "test");
+		sl_put_images(&data);
+		mlx_loop(data.mlx);
 	}
 	else
 		return (TRUE);
